7414a68354207a1b8bdfb1f45987721d4835efc7
Meennuu <meenuawan778@gmail.com>
2024-07-11 14:42:59 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightThemeData(context),
       darkTheme: darkThemeData(context),
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       home: const WelcomeScreen(),
     );
   }
